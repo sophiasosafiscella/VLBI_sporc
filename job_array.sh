f81579474386574391573e05c09fae5910353567
sophiasosafiscella <sophia.sosa@nanograv.org>
2024-08-12 19:24:46 -0400

    Typos

--- job_array.sh
+++ job_array.sh
@@ -14,12 +14,10 @@ cat <<EOF > job_script.sh
 #SBATCH --partition=debug       # Run on tier3
 #SBATCH --output=%x_%j.out      # Output file
 #SBATCH --error=%x_%j.err       # Error file
-#SBATCH --time=1-12:10:00       # 10 minute time limit
+#SBATCH --time=0-00:10:00       # 10 minute time limit
 #SBATCH --ntasks=1              # 1 tasks (i.e. processes)
 #SBATCH --mem-per-cpu=1g        # 1GB RAM per CPU
 #SBATCH --array=0-${n_lines}    # Array size
-
-echo "SLURM_ARRAY_TASK_ID = \${SLURM_ARRAY_TASK_ID}"
 
 # Extract the PMRA for the current SLURM_ARRAY_TASK_ID
 PMRA=\$(awk -v ArrayTaskID=\${SLURM_ARRAY_TASK_ID} '\$1==ArrayTaskID {print \$2}' $config)
@@ -32,7 +30,7 @@ PX=\$(awk -v ArrayTaskID=\${SLURM_ARRAY_TASK_ID} '\$1==ArrayTaskID {print \$4}' 
 
 echo "\${SLURM_ARRAY_TASK_ID}, PMRA = \${PMRA}, PMDEC = \${PMDEC}, PX \${PX}." >> output.txt
 
-srun python3 -u calculate posterior.py \$1 \${SLURM_ARRAY_TASK_ID} \${PMRA} \${PMDEC} \${PX}
+srun python3 -u calculate_posterior.py \$1 \${SLURM_ARRAY_TASK_ID} \${PMRA} \${PMDEC} \${PX}
 
 EOF
 
