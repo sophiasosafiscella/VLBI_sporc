d02f2401556ad8e379ffaee909dee62b8d07bd55
sophiasosafiscella <sophia.sosa@nanograv.org>
2024-08-15 22:20:24 -0400

    Creating minimal script for debugging

--- calculate_posterior_minimal.py
+++ calculate_posterior_minimal.py
@@ -33,3 +33,30 @@ def calculate_post(PSR_name: str, timing_solution, timfile: str, parfile: str, a
 
     print("Successfully converted from ecliptical to equatorial")
     return
+
+if __name__ == "__main__":
+    PSR_name, idx, PMRA, PMDEC, PX = sys.argv[1:]  # Timing solution index and parameters
+
+    import astropy
+    import pint
+
+    print(astropy.__version__)
+    print(pint.__version__)
+
+    timing_solution_dict = {"Index": idx, "PMRA": PMRA, "PMDEC": PMDEC, "PX": PX}
+    for t in pd.DataFrame(timing_solution_dict, columns=list(timing_solution_dict.keys())[1:], index=[timing_solution_dict['Index']]).itertuples(index=True):
+        timing_solution = t
+
+    posteriors_dir: str = f"./results/timing_posteriors/{PSR_name}"
+    astrometric_data_file: str = "./data/astrometric_values.csv"
+
+    # Names of the .tim and .par files
+    timfile: str = glob.glob(f"./data/NG_15yr_dataset/tim/{PSR_name}*tim")[0]
+    parfile: str = glob.glob(f"./data/NG_15yr_dataset/par/{PSR_name}*par")[0]
+
+    # Calculate the posterior
+    posterior = calculate_post(PSR_name, timing_solution, timfile, parfile, astrometric_data_file)
+
+    # Save the timing solution with its posterior
+#    res_np = np.asarray([idx, PMRA, PMDEC, PX, posterior])
+#    np.save(posteriors_dir + "/" + str(idx) + "_posterior.npy", res_np)
