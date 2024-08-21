f2956fe309eec5891b53a2dff44f1b81a48e64d4
sophiasosafiscella <sophia.sosa@nanograv.org>
2024-08-21 10:42:24 -0400

    Updated consolidate_posteriors.py

--- consolidate_posteriors.py
+++ consolidate_posteriors.py
@@ -9,7 +9,7 @@ posteriors_dir: str = f"./results/timing_posteriors/{PSR_name}"
 posterior_files = glob(f"{posteriors_dir}/*posterior.npy")
 
 # Array set to zero but to be replaced with the posteriors
-res_np = np.zeros((len(posterior_files), 3), dtype=float)
+res_np = np.zeros((len(posterior_files), 4), dtype=float)
 idx_np = np.zeros(len(posterior_files), dtype=int)
 
 for i, file in enumerate(posterior_files):
@@ -19,5 +19,5 @@ for i, file in enumerate(posterior_files):
 #    os.remove(file)
 
 # Save the results to a DataFrame
-result_df = pd.DataFrame(data=res_np, index=idx_np, columns=["PMRA", "PMDEC", "PX"]).sort_index()
+result_df = pd.DataFrame(data=res_np, index=idx_np, columns=["PMRA", "PMDEC", "PX", "posterior"]).sort_index()
 result_df.to_pickle(f"./results/timing_posteriors/{PSR_name}_timing_posteriors.pkl")
