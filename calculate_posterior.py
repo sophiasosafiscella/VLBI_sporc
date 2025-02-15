365d831dc38f64ed712e851b6b145d2917d7e6e3
sophiasosafiscella <sophia.sosa@nanograv.org>
2025-02-14 21:51:57 -0500

    Resume chains = False

--- calculate_posterior.py
+++ calculate_posterior.py
@@ -59,7 +59,7 @@ def calculate_post(PSR_name: str, timing_solution, timfile: str, parfile: str, V
     # Perform initial fit
     initial_fit = pint.fitter.DownhillGLSFitter(toas, eq_timing_model)
     try:
-        initial_fit.fit_toas()
+        initial_fit.fit_toas(maxiter=5)
     except:
         print("Timing solution failed")
         return [[0.0]]
@@ -140,7 +140,7 @@ if __name__ == "__main__":
     parfile: str = glob.glob(f"./data/NG_15yr_dataset/par/{PSR_name}_PINT*par")[0]
 
     # Calculate the posterior
-    posterior = calculate_post(PSR_name, timing_solution, timfile, parfile, VLBI_astrometric_data_file, resume=False, plot=False)[0][0]
+    posterior = calculate_post(PSR_name, timing_solution, timfile, parfile, VLBI_astrometric_data_file, resume=True, plot=False)[0][0]
 
     # Save the timing solution with its posterior
     res_np = np.asarray([idx, RAJ, DECJ, PMRA, PMDEC, PX, posterior])
