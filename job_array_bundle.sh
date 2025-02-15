9746efb2e01c67686d3e85011ca4b7ba165c3497
sophiasosafiscella <sophia.sosa@nanograv.org>
2025-02-15 11:13:38 -0500

    When finding the overlap, "PMRA" and "PMDEC" are added AT THE END of the table

--- job_array_bundle.sh
+++ job_array_bundle.sh
@@ -47,10 +47,10 @@ else
 fi
 
 # Read only from line 2 onwards
-awk "NR>=\$start && NR<=\$end" "\$config" | while read -r ArrayTaskID RAJ DECJ PMRA PMDEC PX; do
-    echo "\${PSR_name}, \${ArrayTaskID}, RAJ = \${RAJ}, DECJ = \${DECJ}, PMRA = \${PMRA}, PMDEC = \${PMDEC}, PX = \${PX}." >> output.txt
+awk "NR>=\$start && NR<=\$end" "\$config" | while read -r ArrayTaskID RAJ DECJ PX PMRA PMDEC; do
+    echo "\${PSR_name}, \${ArrayTaskID}, RAJ = \${RAJ}, DECJ = \${DECJ}, PX = \${PX}, PMRA = \${PMRA}, PMDEC = \${PMDEC}." >> output.txt
 
-    srun --mem-per-cpu=10g python3 -u calculate_posterior.py "\${PSR_name}" "\${ArrayTaskID}" "\${RAJ}" "\${DECJ}" "\${PMRA}" "\${PMDEC}" "\${PX}"
+    srun --mem-per-cpu=10g python3 -u calculate_posterior.py "\${PSR_name}" "\${ArrayTaskID}" "\${RAJ}" "\${DECJ}" "\${PX}" "\${PMRA}" "\${PMDEC}"
 done
 
 EOF
