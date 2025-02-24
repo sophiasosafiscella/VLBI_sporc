ad425bcbfe95a65551682a677d8170b1377dd449
sophiasosafiscella <sophia.sosa@nanograv.org>
2025-02-24 17:51:15 -0500

    Make plots

--- plot_results.py
+++ plot_results.py
@@ -18,49 +18,52 @@ for row, PSR_name in enumerate(PSR_names):
     posteriors_file: str = f"./results/timing_posteriors_frame_tie/{PSR_name}_consolidated_timing_posteriors.pkl"
 
     # Load the posteriors
-    result_df = pd.read_pickle(posteriors_file)
-    result_df = result_df.rename(columns={'PMRA': 'PX', 'PMDEC': 'PMRA', 'PX': 'PMDEC'})
+    df = pd.read_pickle(posteriors_file)
+
+    # Convert PX, PMRA, PMDEC to float
+    df[["PX", "PMRA", "PMDEC", "posterior"]] = df[["PX", "PMRA", "PMDEC", "posterior"]].astype(float)
 
     # Find the solution with the highest posterior
-    best_sol_idx = result_df['posterior'].idxmax()
-    best_sol = result_df.iloc[best_sol_idx].to_dict()
+    best_sol_idx = df['posterior'].idxmax()
+    best_sol = df.iloc[best_sol_idx].to_dict()
 
     # RAJ
-    sols = result_df[(result_df['PX'] == best_sol['PX']) & (result_df['DECJ'] == best_sol['DECJ']) &
-                     (result_df['PMRA'] == best_sol['PMRA']) & (result_df['PMDEC'] == best_sol['PMDEC'])]
+    sols = df[(df['PX'] == best_sol['PX']) & (df['DECJ'] == best_sol['DECJ']) &
+              (df['PMRA'] == best_sol['PMRA']) & (df['PMDEC'] == best_sol['PMDEC'])]
 
     timing_RAJ = Angle(timing_astrometric_data.loc[PSR_name, "ra_t"], unit=u.hourangle)
     ref_RAJ = Angle(f"{int(timing_RAJ.hms[0])}h{int(timing_RAJ.hms[1])}m{round(timing_RAJ.hms[2], 1)}s")
 
     timing_deltaRAJ_ms = (timing_RAJ - ref_RAJ).hms[2] * 1000.0
-
     deltaRAJ_ms = [(Angle(x, unit=u.hourangle) - ref_RAJ).hms[2] * 1000.0 for x in sols["RAJ"]]
 
     fig.add_trace(go.Scatter(x=deltaRAJ_ms, y=sols["posterior"], mode='lines+markers', marker=dict(color=VLBI_color)),
                   row=row+1, col=1)
-    fig.add_vline(x=timing_deltaRAJ_ms, line_width=3, line_dash="dash",
-                  line_color=timing_color, row=row+1, col=1)
+    fig.add_vline(x=timing_deltaRAJ_ms, line_width=3, line_dash="dash", line_color=timing_color, row=row+1, col=1)
     fig.update_yaxes(showticklabels=False, row=row+1, col=1)
     fig.update_xaxes(title_text="$\mathrm{RAJ} - " + f"{ref_RAJ:latex}"[1:-1] + " [\mathrm{mas}]$", row=row+1, col=1)
 
     # DECJ
-    sols = result_df[(result_df['RAJ'] == best_sol['RAJ']) & (result_df['PX'] == best_sol['PX']) &
-                     (result_df['PMRA'] == best_sol['PMRA']) & (result_df['PMDEC'] == best_sol['PMDEC'])]
-    fig.add_trace(go.Scatter(x=sols["DECJ"], y=sols["posterior"], mode='lines+markers', marker=dict(color=VLBI_color)),
+    sols = df[(df['RAJ'] == best_sol['RAJ']) & (df['PX'] == best_sol['PX']) &
+              (df['PMRA'] == best_sol['PMRA']) & (df['PMDEC'] == best_sol['PMDEC'])]
+
+    timing_DECJ = Angle(timing_astrometric_data.loc[PSR_name, "dec_t"], unit=u.degree)
+    ref_DECJ = Angle(f"{int(timing_DECJ.dms[0])}d{int(abs(timing_DECJ.dms[1]))}m{round(abs(timing_DECJ.dms[2]), 1)}s")
+
+    timing_deltaDECJ_ms = (timing_DECJ - ref_DECJ).dms[2] * 1000.0
+    deltaDECJ_ms = [(Angle(x, unit=u.degree) - ref_DECJ).dms[2] * 1000.0 for x in sols["DECJ"]]
+
+    fig.add_trace(go.Scatter(x=deltaDECJ_ms, y=sols["posterior"], mode='lines+markers', marker=dict(color=VLBI_color)),
                   row=row+1, col=2)
-    fig.add_vline(x=timing_astrometric_data.loc[PSR_name, "dec_t"], line_width=3, line_dash="dash",
-                  line_color=timing_color, row=row+1, col=2)
+    fig.add_vline(x=timing_deltaDECJ_ms, line_width=3, line_dash="dash", line_color=timing_color, row=row+1, col=2)
     fig.update_yaxes(showticklabels=False, row=row+1, col=2)
-    fig.update_xaxes(title_text="DECJ [d:m:s]", row=row+1, col=2)
+    fig.update_xaxes(title_text="$\mathrm{DECJ} - (" + f"{ref_DECJ:latex}"[1:-1] + ") [\mathrm{mas}]$", row=row+1, col=2)
 
     # PX
-    sols = result_df[(result_df['RAJ'] == best_sol['RAJ']) & (result_df['DECJ'] == best_sol['DECJ']) &
-                     (result_df['PMRA'] == best_sol['PMRA']) & (result_df['PMDEC'] == best_sol['PMDEC'])]
+    sols = df[(df['RAJ'] == best_sol['RAJ']) & (df['DECJ'] == best_sol['DECJ']) &
+              (df['PMRA'] == best_sol['PMRA']) & (df['PMDEC'] == best_sol['PMDEC'])]
 
-    sols_PX = sols["PX"].astype(float)
-    sols_post = sols["posterior"].astype(float)
-
-    fig.add_trace(go.Scatter(x=sols_PX, y=sols_post, mode='lines+markers', marker=dict(color=VLBI_color)), row=row+1,
+    fig.add_trace(go.Scatter(x=sols["PX"], y=sols["posterior"], mode='lines+markers', marker=dict(color=VLBI_color)), row=row+1,
                   col=3)
     fig.add_vline(x=timing_astrometric_data.loc[PSR_name, "px_t"], line_width=3, line_dash="dash",
                   line_color=timing_color, row=row+1, col=3)
@@ -68,13 +71,10 @@ for row, PSR_name in enumerate(PSR_names):
     fig.update_xaxes(title_text="PX [mas]", row=row+1, col=3)
 
     # PMRA
-    sols = result_df[(result_df['RAJ'] == best_sol['RAJ']) & (result_df['DECJ'] == best_sol['DECJ']) &
-                     (result_df['PX'] == best_sol['PX']) & (result_df['PMDEC'] == best_sol['PMDEC'])]
+    sols = df[(df['RAJ'] == best_sol['RAJ']) & (df['DECJ'] == best_sol['DECJ']) &
+              (df['PX'] == best_sol['PX']) & (df['PMDEC'] == best_sol['PMDEC'])]
 
-    sols_PMRA = sols["PMRA"].astype(float)
-    sols_post = sols["posterior"].astype(float)
-
-    fig.add_trace(go.Scatter(x=sols_PMRA, y=sols_post, mode='lines+markers', marker=dict(color=VLBI_color)), row=row+1,
+    fig.add_trace(go.Scatter(x=sols["PMRA"], y=sols["posterior"], mode='lines+markers', marker=dict(color=VLBI_color)), row=row+1,
                   col=4)
     fig.add_vline(x=timing_astrometric_data.loc[PSR_name, "pmra_t"], line_width=3, line_dash="dash",
                   line_color=timing_color, row=row+1, col=4)
@@ -82,25 +82,24 @@ for row, PSR_name in enumerate(PSR_names):
     fig.update_xaxes(title_text="PMRA [mas/yr]", row=row+1, col=4)
 
     # PMDEC
-    sols = result_df[(result_df['RAJ'] == best_sol['RAJ']) & (result_df['DECJ'] == best_sol['DECJ']) &
-                     (result_df['PMRA'] == best_sol['PMRA']) & (result_df['PX'] == best_sol['PX'])]
+    sols = df[(df['RAJ'] == best_sol['RAJ']) & (df['DECJ'] == best_sol['DECJ']) &
+              (df['PMRA'] == best_sol['PMRA']) & (df['PX'] == best_sol['PX'])]
 
-    sols_PMDEC = sols["PMDEC"].astype(float)
-    sols_post = sols["posterior"].astype(float)
-
-    fig.add_trace(go.Scatter(x=sols_PMDEC, y=sols_post, mode='lines+markers', marker=dict(color=VLBI_color)), row=row+1,
+    fig.add_trace(go.Scatter(x=sols["PMDEC"], y=sols["posterior"], mode='lines+markers', marker=dict(color=VLBI_color)), row=row+1,
                   col=5)
     fig.add_vline(x=timing_astrometric_data.loc[PSR_name, "pmdec_t"], line_width=3, line_dash="dash",
                   line_color=timing_color, row=row+1, col=5)
     fig.update_yaxes(showticklabels=False, row=row+1, col=5)
     fig.update_xaxes(title_text="PMDEC [mas/yr]", row=row+1, col=5)
 
+    fig.update_yaxes(title_text=PSR_name, row=row + 1, col=1)
+
     fig.update_xaxes(tickformat=".3f")
     fig.update_layout(showlegend=False)
     fig.update_layout(
         autosize=False,
         width=1200,
-        height=800,
+        height=700,
     )
 
 fig.show()
--- plot_results_2D.py
+++ plot_results_2D.py
@@ -2,35 +2,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from astropy.coordinates import Angle
 from pint.models import get_model
 from scipy.interpolate import griddata
+from itertools import product
 import glob
+import sys
 
-def plot_contour(df, tm, timing_data, x_col, y_col, w_col, ax):
+def plot_contour(df, best_sol, timing_astrometric_data, tm, x_label, y_label, ax):
 
-    params = ["RAJ", "DECJ", "PX", "PMRA", "PMDEC"]
-    # Extract columns
-    x = df[x_col]
-    y = df[y_col]
-    w = df[w_col]
+#    all_params = ["RAJ", "DECJ", "PX", "PMRA", "PMDEC"]
+#    p = [param for param in all_params if param not in (x_label, y_label)]
+
+#    sols = df[(df[p[0]] == best_sol[p[0]]) & (df[p[1]] == best_sol[p[1]]) & (df[p[2]] == best_sol[p[2]])]
+
+    x_values, y_values = df[x_label].unique(), df[y_label].unique()
+    z_values = np.zeros((len(y_values), len(x_values)))
+
+    for i, x in enumerate(x_values):
+        for j, y in enumerate(y_values):
+            z_values[j, i] = df[(df[x_label] == x) & (df[y_label] == y)]['posterior'].sum()
 
     # Create grid values first
-    xi = np.linspace(x.min(), x.max(), 100)
-    yi = np.linspace(y.min(), y.max(), 100)
-    xi, yi = np.meshgrid(xi, yi)
+#    xi = np.linspace(x.min(), x.max(), 100)
+#    yi = np.linspace(y.min(), y.max(), 100)
+#    xi, yi = np.meshgrid(xi, yi)
 
     # Interpolate w values on grid
-    zi = griddata((x, y), w, (xi, yi), method='linear')
+#    zi = griddata((x, y), w, (xi, yi), method='linear')
 
     # Plot contour
-    contour = ax.contourf(xi, yi, zi, levels=15, cmap="viridis")
-    plt.colorbar(contour, ax=ax, label=w_col)
+    contour = ax.contourf(x_values, y_values, z_values, levels=15, cmap="viridis")
+    plt.colorbar(contour, ax=ax, label='posterior')
 
     # Extract the reference timing values
-    ax.scatter(timing_data[x_col], timing_data[y_col], marker='x', c='red', s=400)
+#    ax.scatter(timing_data[x_col], timing_data[y_col], marker='x', c='red', s=400)
 
-    ax.set_xlabel(f"{x_col} [{getattr(tm, x_col).units}]")
-    ax.set_ylabel(f"{y_col} [{getattr(tm, y_col).units}]")
+    ax.set_xlabel(f"{x_label} [{getattr(tm, x_label).units}]")
+    ax.set_ylabel(f"{y_label} [{getattr(tm, y_label).units}]")
 #    ax.set_title(f'{x_col} vs {y_col} with {w_col} as color')
 
 
@@ -39,12 +48,19 @@ if __name__ == "__main__":
     PSR_name: str = "J0030+0451"
     posteriors_file: str = f"./results/timing_posteriors_frame_tie/{PSR_name}_consolidated_timing_posteriors.pkl"
 
+    # Get the nominal timing values
+    parfile: str = glob.glob(f"./data/NG_15yr_dataset/par/{PSR_name}*par")[0]
+    ec_timing_model = get_model(parfile)  # Ecliptical coordiantes
+    tm = ec_timing_model.as_ICRS(epoch=ec_timing_model.POSEPOCH.value)
+
     # Load the timing solution
     timing_astrometric_data = pd.read_csv("./data/timing_astrometric_data_updated.csv", index_col=0, header=0)
 
     # Load the posteriors
     result_df = pd.read_pickle(posteriors_file)
-    result_df = result_df.rename(columns={'PMRA': 'PX', 'PMDEC': 'PMRA', 'PX': 'PMDEC'})
+
+    # Convert PX, PMRA, PMDEC to float
+    result_df[["PX", "PMRA", "PMDEC", "posterior"]] = result_df[["PX", "PMRA", "PMDEC", "posterior"]].astype(float)
 
     # Find the solution with the highest posterior
     best_sol_idx = result_df['posterior'].idxmax()
@@ -53,20 +69,33 @@ if __name__ == "__main__":
     # Create subplots
     sns.set_context('poster')
     sns.set_style('ticks')
-    fig, axs = plt.subplots(5, 5, figsize=(18, 12))
+    fig, axs = plt.subplots(4, 4, figsize=(36, 30))
     fig.suptitle(PSR_name)
 
-    for row in range(5):
-        for col in range(row+1, 5):
+    for row in range(4):
+
+        for col in range(row+1, 4):
             axs[row, col].axis('off')
 
+        for col in range(4):
+            if col > 0:
+                axs[row, col].get_yaxis().set_visible(False)
+            if row < 3:
+                axs[row, col].get_xaxis().set_visible(False)
+
+
+
     # Plot each pair
-    plot_contour(df, eq_timing_model, timing_data, 'RAJ', 'DECJ', 'posterior', axs[0, 0])
-    plot_contour(df, eq_timing_model, timing_data, 'RAJ', 'PMRA', 'posterior', axs[1, 0])
-    plot_contour(df, eq_timing_model, timing_data, 'RAJ', 'PMDEC', 'posterior', axs[2, 0])
-    plot_contour(df, eq_timing_model, timing_data, 'RAJ', 'PX', 'posterior', axs[3, 0])
-    plot_contour(df, eq_timing_model, timing_data, 'RAJ', 'PX', 'posterior', axs[4, 0])
-    plot_contour(df, eq_timing_model, timing_data,'PMDEC', 'PX', 'posterior', axs[1, 1])
+    plot_contour(result_df, best_sol, timing_astrometric_data, tm, 'RAJ', 'DECJ', axs[0, 0])
+    plot_contour(result_df, best_sol, timing_astrometric_data, tm, 'RAJ', 'PMRA', axs[1, 0])
+    plot_contour(result_df, best_sol, timing_astrometric_data, tm, 'DECJ', 'PMRA', axs[1, 1])
+    plot_contour(result_df, best_sol, timing_astrometric_data, tm, 'RAJ', 'PMDEC', axs[2, 0])
+    plot_contour(result_df, best_sol, timing_astrometric_data, tm, 'DECJ', 'PMDEC', axs[2, 1])
+    plot_contour(result_df, best_sol, timing_astrometric_data, tm, 'PMRA', 'PMDEC', axs[2, 2])
+    plot_contour(result_df, best_sol, timing_astrometric_data, tm, 'RAJ', 'PX', axs[3, 0])
+    plot_contour(result_df, best_sol, timing_astrometric_data, tm, 'DECJ', 'PX', axs[3, 1])
+    plot_contour(result_df, best_sol, timing_astrometric_data, tm, 'PMRA', 'PX', axs[3, 2])
+    plot_contour(result_df, best_sol, timing_astrometric_data, tm, 'PMDEC', 'PX', axs[3, 3])
 
     plt.tight_layout()
     plt.show()
