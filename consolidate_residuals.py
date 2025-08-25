23da9a412073fdf30b71395ca8c29193419a487e
sophiasosafiscella <sophia.sosa@nanograv.org>
2025-08-25 15:02:22 -0400

    Finished creating consolidate_residuals.py

--- consolidate_residuals.py
+++ consolidate_residuals.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from statistics import harmonic_mean
 from astropy.timeseries import LombScargle
 import astropy.units as u
@@ -41,33 +42,64 @@ def weighted_median(series, weights):
     return series[i]
 
 PSR_name: str = sys.argv[1]
-posteriors_dir: str = f"./results/timing_posteriors_frame_tie/{PSR_name}"
-posterior_files = glob(f"{posteriors_dir}/*posterior.npy")
-residual_files = glob(f"{posteriors_dir}/*residuals_diff.npy")
+results_dir: str = f"./results/timing_posteriors_frame_tie/{PSR_name}"
+results_files = glob(f"{results_dir}/*results.npy")
+n_timing_solutions = len(results_files)
 
 # Array set to zero but to be replaced with the posteriors
-nbins: int = np.shape(np.load(residual_files[0:1]))[0]
-res_np = np.zeros((len(residual_files), nbins), dtype=object)
-posteriors_np = np.zeros(len(posterior_files), dtype=float)
+posteriors_arr = np.zeros(n_timing_solutions, dtype=float)
 
-for i, (res_file, post_file) in enumerate(zip(residual_files,posterior_files)):
+# Try the first file:
+results = pd.read_pickle(results_files[0:1])
+res_diffs = results.residuals_diff[0]
+res_diff_nominal_values = np.asarray([res_diffs.nominal_value for x in res_diffs])
+epochs = results.res_epochs[0]
 
+# Lomb-Scargle Periodogram
+frequencies, powers = LombScargle(epochs * u.day, res_diff_nominal_values * u.us).autopower()
+powers_arr = np.empty((n_timing_solutions, len(powers)), dtype=float)
+n_freqs = len(frequencies)
+
+for i, file in enumerate(results_files):
+
+    # Extract results
+    results = pd.read_pickle(file)
+    res_diffs = results.residuals_diff[0]
+    res_diff_nominal_values = np.asarray([res_diffs.nominal_value for x in res_diffs])
+    epochs = results.res_epochs[0]
+    posteriors_arr[i] = results.posterior[0]
 
     # Lomb-Scargle Periodogram
-    avg_residuals = np.load(res_file)
-    frequency, power = LombScargle(epochs * u.day, avg_residuals * u.us).autopower()
-
-
-
-    posteriors_np[i] = np.load(post_file)[-1]
+    freqs, powers_arr[i, :] = LombScargle(epochs * u.day, res_diff_nominal_values * u.us).autopower()
+    if len(freqs) != n_freqs:
+        sys.exit("Error in the number of frequencies")
 
 # Normalize the posteriors
-posteriors_np /= np.amax(posteriors_np)
+posteriors_arr /= np.amax(posteriors_arr)
+print("Normalized posteriors: " + str(posteriors_arr))
+
+# Calculate the weighted mean of the periodograms per frequency
+weightedmean_arr = np.empty(n_freqs, dtype=float)
+std_dev_arr = np.empty(n_freqs, dtype=float)
+
+for i in len(frequencies):
+    weightedmean_arr[i], std_dev_arr[i] = weighted_moments(series=powers_arr[:, i], weights=posteriors_arr[i])
+
+# Plot the results
+fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(10, 14)) #, gridspec_kw = {'wspace':0, 'hspace':0})
+axs.plot(frequencies, weightedmean_arr)
+axs.axvline(x=1.0/((1 * u.yr).to(u.day)).value, color='C1', linestyle='--', lw=2.0, label="$(\mathrm{1~year})^{-1}$")
+axs.axvline(x=1.0/((0.5 * u.yr).to(u.day)).value, color='C2', linestyle='--', lw=2.0, label="$(\mathrm{6~months})^{-1}$")
+axs.set_xscale('log')
+axs.set_yscale('log')
+axs.set_xlabel('Frequency [1/day]')
+axs.set_ylabel('Log(Power) [$\mathrm{\mu s^2}$/day]')
+axs.legend(loc='best')
+axs.grid()
 
 
+plt.tight_layout()
+plt.savefig(results_dir + f"/{PSR_name}_periodogram.pdf")
+plt.show()
 
-# Take the weighted sum of the individual periodograms
 
-# Save the results to a DataFrame
-result_df = pd.DataFrame(data=res_np, columns=["POSEPOCH", "RAJ", "DECJ", "PX", "PMRA", "PMDEC", "posterior"]).sort_index()
-result_df.to_pickle(f"./results/timing_posteriors_frame_tie/{PSR_name}_consolidated_timing_residuals.pkl")
